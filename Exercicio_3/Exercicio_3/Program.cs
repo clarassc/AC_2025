2a3944da9be9d10a294988ce73dd3585cc0ac428
clarassc <clara250406@gmail.com>
2025-03-12 10:03:30 +0000

    Add files via upload

---
+++ Exercicio_3/Exercicio_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Exercicio3
+{
+    public static void Main()
+    {
+        // Solicita a idade
+        Console.Write("Digite sua idade: ");
+        int idade = Convert.ToInt32(Console.ReadLine());
+
+        // Solicita se é estudante
+        Console.Write("És estudante? (sim/não): ");
+        string resposta = Console.ReadLine();
+
+        // Converte a resposta para booleano
+        bool isEstudante = (resposta == "sim");
+
+        // Verifica se é estudante adulto
+        if (idade >= 18 && isEstudante)
+        {
+            Console.WriteLine("É um estudante adulto.");
+        }
+        else
+        {
+            Console.WriteLine("Não é um estudante adulto.");
+        }
+    }
+}
