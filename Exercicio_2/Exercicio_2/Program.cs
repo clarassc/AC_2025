71c23f84a73493561d9e447cf240fda2667f7b1c
clarassc <clara250406@gmail.com>
2025-03-12 10:03:00 +0000

    Add files via upload

---
+++ Exercicio_2/Exercicio_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Pede o raio
+        Console.Write("Digite o raio da circunferência: ");
+        double raio = Convert.ToDouble(Console.ReadLine());
+
+        // Calcula a área (π * raio²)
+        double area = Math.PI * (raio * raio);
+
+        // Resultado
+        Console.WriteLine("O valor da área de raio " + raio + " é " + Math.Round(area, 0) + "m2");
+    }
+}
